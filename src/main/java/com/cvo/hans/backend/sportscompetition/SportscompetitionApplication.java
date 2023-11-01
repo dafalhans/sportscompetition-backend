0e17fb528357d3902b34f36dd1ed2e3d2fac46c7
hans <hans.vandeweghe@gmail.com>
2023-11-01 18:13:46 +0100

    structure FE and BE

---
+++ src/main/java/com/cvo/hans/backend/sportscompetition/SportscompetitionApplication.java
@@ -0,0 +1,13 @@
+package com.cvo.hans.backend.sportscompetition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportscompetitionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportscompetitionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cvo/hans/backend/sportscompetition/SportscompetitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cvo.hans.backend.sportscompetition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportscompetitionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
